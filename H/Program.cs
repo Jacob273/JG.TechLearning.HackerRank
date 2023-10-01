0949ffbb41bec2aa8a5212d3fc25e410a33c7891
Jakub G <jakubgmur@op.pl>
2023-10-01 14:23:44 +0200

    added Reverse method for RotatorV2

--- H/Program.cs
+++ H/Program.cs
@@ -9,3 +9,9 @@ RotatorV1.RotateRight(arr, 1);
 Assert.IsTrue(new int[] { 4, 5, 1, 2, 3 }.SequenceEqual(arr), "Not equal");
 RotatorV1.RotateRight(arr, 3);
 Assert.IsTrue(new int[] { 1, 2, 3, 4, 5 }.SequenceEqual(arr), "Not equal");
+
+
+
+int[] arr2 = { 1, 2, 3, 4, 5 };
+RotatorV2.Reverse(arr2, 0, 4);
+Assert.IsTrue(new int[] { 5, 4, 3, 2, 1 }.SequenceEqual(arr2), "Not equal");
---
+++ H/RotatorV2.cs
@@ -0,0 +1,28 @@
+﻿namespace H
+{
+    /// <summary>
+    /// Time complexity: Big_O(n).
+    /// </summary>
+    public static class RotatorV2
+    {
+        public static void RotateLeft(int[] array, int d)
+        {
+            var newD = d % array.Length;
+
+            // three traversals (first from 0 do d -1)
+            Reverse(array, startIndex: 0, endIndex: d - 1);
+
+        }
+
+        public static void Reverse(int[] array, int startIndex, int endIndex)
+        {
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                int temp = array[i];
+                array[i] = array[endIndex];
+                array[endIndex] = temp;
+                endIndex--;
+            }
+        }
+    }
+}
