994c1d8df2072f5beac89e1e2775fad71b66eee6
Jakub G <jakubgmur@op.pl>
2023-10-01 14:40:50 +0200

    added optimized rotation v2

--- H/RotatorV2.cs
+++ H/RotatorV2.cs
@@ -1,17 +1,28 @@
 ﻿namespace H
 {
     /// <summary>
-    /// Time complexity: Big_O(n).
+    /// Time complexity: Big_O(n), three separate reversal operations where each take Big_O(n).
     /// </summary>
     public static class RotatorV2
     {
         public static void RotateLeft(int[] array, int d)
         {
-            var newD = d % array.Length;
+            d = d % array.Length;
 
-            // three traversals (first from 0 do d -1)
+            // three traversals
             Reverse(array, startIndex: 0, endIndex: d - 1);
+            Reverse(array, startIndex: d, endIndex: array.Length - 1);
+            Reverse(array, startIndex: 0, array.Length - 1);
+        }
 
+        public static void RotateRight(int[] array, int d)
+        {
+            d = d % array.Length;
+
+            // three traversals
+            Reverse(array, startIndex: 0, endIndex: array.Length - d - 1);
+            Reverse(array, startIndex: array.Length - d, endIndex: array.Length - 1);
+            Reverse(array, startIndex: 0, array.Length - 1);
         }
 
         public static void Reverse(int[] array, int startIndex, int endIndex)
