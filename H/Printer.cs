47055aa70c571f0c9c81980ef0bb530c171e7c3b
Jakub G <jakubgmur@op.pl>
2023-10-01 13:48:09 +0200

    moved classes outside of Progrma.cs

---
+++ H/Printer.cs
@@ -0,0 +1,14 @@
+﻿namespace H
+{
+    public static class Printer
+    {
+        public static void PrintArr(int[] arr)
+        {
+            foreach (var element in arr)
+            {
+                Console.Write(element);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ H/Rotator.cs
@@ -0,0 +1,33 @@
+﻿namespace H
+{
+    public static class Rotator
+    {
+        public static void RotateLeft(int[] array, int d)
+        {
+            var newD = d % array.Length;
+            for (int i = 0; i < newD; i++)
+            {
+                int first = array[0];
+                for (int j = 1; j < array.Length; j++)
+                {
+                    array[j - 1] = array[j];
+                }
+                array[array.Length - 1] = first;
+            }
+        }
+
+        public static void RotateRight(int[] array, int d)
+        {
+            var newD = d % array.Length;
+            for (int i = 0; i < newD; i++)
+            {
+                int last = array[array.Length - 1];
+                for (int j = array.Length - 1; j > 0; j--)
+                {
+                    array[j] = array[j - 1];
+                }
+                array[0] = last;
+            }
+        }
+    }
+}
