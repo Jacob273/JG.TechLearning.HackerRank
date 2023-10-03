f5a5f352ecbee8a37e1c9c2ced5c283edacd9a89
Jakub G <jakubgmur@op.pl>
2023-10-04 01:07:59 +0200

    added time complexity

--- H/AnagramCheckerV2.cs
+++ H/AnagramCheckerV2.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// This class checks whether word is anagram.
     /// An anagram is a word or phrase formed by rearranging the letters of another.
-    /// Time complexity: ?
+    /// Time complexity: Big_O(n)
     /// </summary>
     public static class AnagramCheckerV2
     {
@@ -11,12 +11,12 @@
         public static bool IsAngram(string str1, string str2)
         {
             var frequencies1 = new Dictionary<char, int>();
-            CountChars(str1, frequencies1);
+            CountChars(str1, frequencies1); // iterating over each character, Big_O(n)
 
             var frequencies2 = new Dictionary<char, int>();
-            CountChars(str2, frequencies2);
+            CountChars(str2, frequencies2); // iterating over each character, Big_O(n)
 
-            foreach (var kvp1 in frequencies1)
+            foreach (var kvp1 in frequencies1) // iterating over each frequency registered, Big_O(n)
             {
                 if(frequencies2.TryGetValue(kvp1.Key, out int freqCount) && freqCount == kvp1.Value)
                 {
